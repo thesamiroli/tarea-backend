a55ac2964e16e0b9706bc022e1f2c65c92a86a90
Samir Oli <samiroli789@gmail.com>
2020-05-14 21:28:22 +0545

    Remove logs

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -3,15 +3,10 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
-    console.log(req.headers);
-    console.log("Authorization 6, Token: ", token);
     const decoded = jwt.verify(token, "randomKey123");
-    console.log("Authorization 8, Decoded Data: ", decoded);
     req.userData = decoded;
-    console.log("Authorization 10, Token Verified");
     next();
   } catch (error) {
-    console.log("Authorization 13, Token Verification Failed");
     next({ status: 401, message: "Token Verification Failed" });
   }
 };
